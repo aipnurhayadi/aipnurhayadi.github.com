28420044c2e1ca5ba303c364fc5aba051aeaa6fa
Aipnur <aipnurhayadi08@gmail.com>
2021-12-16 23:54:08 +0700

    initial commit

---
+++ components/ProfileIndex.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="h-screen flex items-center justify-center bg-gray-50">
+    <div class="text-2xl text-gray-700 text-content">UNDER CONSTRUCTION</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProfileIndex',
+}
+</script>
+
+<style scoped>
+.text-content {
+  font-family: 'Inter';
+}
+</style>
